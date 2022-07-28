2fecee05a9e9d2efa7feede9d2e44a05117075f1
tejuypatil <tejuypatil@gmail.com>
2022-07-28 17:39:06 +0530

    Added program of Deck of cards

--- src/com/bridgelab/oops/deckcards/DeckOfCards.java
+++ src/com/bridgelab/oops/deckcards/DeckOfCards.java
@@ -1,4 +1,29 @@
 package com.bridgelab.oops.deckcards;
 
 public class DeckOfCards {
+    public static void main(String[] args) {
+        String[] suit = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] rank = {"2", "3", "4", "5", "6", "7", "8","9", "10", "Jack", "Queen", "King", "Ace"};
+        String[][] cardsArray = new String[4][13];
+
+        for(int i=0;i<4;i++){
+            for (int j=0;j<13;j++){
+                cardsArray[i][j] =suit[i]+' '+ rank[j];
+            }
+        }
+        ShuffleCards shuffle = new ShuffleCards();
+        shuffle.shuffleCards(cardsArray);
+        for (int i=0;i<4;i++){
+            System.out.println();
+            System.out.println("Cards for player "+(i+1));
+            System.out.println();
+            for (int j=0;j<9;j++){
+                System.out.println(cardsArray[i][j]);
+            }
+
+        }
+    }
 }
+
+
+
---
+++ src/com/bridgelab/oops/deckcards/ShuffleCards.java
@@ -0,0 +1,17 @@
+package com.bridgelab.oops.deckcards;
+
+public class ShuffleCards {
+    public void shuffleCards(String[][] arr){
+        String temp;
+        for (int i =0;i<4;i++) {
+            for (int j = 0;j<13;j++) {
+                int a = (int)(Math.random()*10)%4;
+                int b = (int)(Math.random()*10)%13;
+                temp = arr[i][j];
+                arr[i][j] = arr[a][b];
+                arr[a][b] = temp;
+            }
+        }
+    }
+
+}
